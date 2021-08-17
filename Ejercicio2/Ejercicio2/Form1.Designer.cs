746e73f72769198ecee7ca377b5973743004189d
Alexander Elías <81264666+Alexander0076@users.noreply.github.com>
2021-08-16 19:29:58 -0600

    Add files via upload

---
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmb2 = new System.Windows.Forms.ComboBox();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "General Fecha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccione el día";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Seleccione el mes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese el año";
+            // 
+            // cmb2
+            // 
+            this.cmb2.FormattingEnabled = true;
+            this.cmb2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.cmb2.Location = new System.Drawing.Point(304, 81);
+            this.cmb2.Name = "cmb2";
+            this.cmb2.Size = new System.Drawing.Size(121, 21);
+            this.cmb2.TabIndex = 4;
+            // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Items.AddRange(new object[] {
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"});
+            this.cmb1.Location = new System.Drawing.Point(304, 132);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(121, 21);
+            this.cmb1.TabIndex = 5;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(304, 178);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(121, 20);
+            this.txt2.TabIndex = 6;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(304, 266);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(132, 20);
+            this.txt3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Generar fecha";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 402);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.cmb2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmb2;
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ejercicio2/Ejercicio2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        private string mes;
+        private int year, dia;
+        private int[] numeros = new int[10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dia = Convert.ToInt32(cmb2.Text);
+            mes = Convert.ToString(cmb1.Text);
+            year = Convert.ToInt32(txt2.Text);
+
+            if (DateTime.IsLeapYear(year) == true)
+            {
+
+
+                switch (mes)
+                {
+                    case "Enero":
+                        break;
+                    case "Febrero":
+                        if (dia == 29)
+                        {
+                            MessageBox.Show("El dia selecionado no es correcto ya que el año es bisiesto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            txt3.Text = dia + "/" + mes + "/" + year;
+                        }
+
+                        break;
+                    case "Marzo":
+                        break;
+                    case "Abril":
+                        break;
+                    case "Mayo":
+                        break;
+                    case "Junio":
+                        break;
+                    case "Julio":
+                        break;
+                    case "Agosto":
+                        break;
+                    case "Septiembre":
+                        break;
+                    case "Octubre":
+                        break;
+                    case "Noviembre":
+                        break;
+                    case "Diciembre":
+                        break;
+                }
+
+            }
+            else { MessageBox.Show("El día seleccionado en el mes, no es valido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+    }
+}
