b2715cb7f5f293b5649d2f9f3a5514231b61a4b0
Alexander0076 <uliseselias007@gmail.com>
2021-08-17 03:02:40 -0600

    Nuevo ejercicio1

---
+++ Ejercicio1new/Ejercicio1new/Form1.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace Ejercicio1new
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.TextBox();
+            this.n3 = new System.Windows.Forms.TextBox();
+            this.n4 = new System.Windows.Forms.TextBox();
+            this.n5 = new System.Windows.Forms.TextBox();
+            this.n6 = new System.Windows.Forms.TextBox();
+            this.n7 = new System.Windows.Forms.TextBox();
+            this.n8 = new System.Windows.Forms.TextBox();
+            this.n9 = new System.Windows.Forms.TextBox();
+            this.n10 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.md = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.maxi = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.mini = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.md2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(125, 61);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(100, 20);
+            this.n1.TabIndex = 0;
+            // 
+            // n2
+            // 
+            this.n2.Location = new System.Drawing.Point(125, 111);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(100, 20);
+            this.n2.TabIndex = 1;
+            // 
+            // n3
+            // 
+            this.n3.Location = new System.Drawing.Point(125, 160);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(100, 20);
+            this.n3.TabIndex = 2;
+            // 
+            // n4
+            // 
+            this.n4.Location = new System.Drawing.Point(125, 205);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(100, 20);
+            this.n4.TabIndex = 3;
+            // 
+            // n5
+            // 
+            this.n5.Location = new System.Drawing.Point(125, 250);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(100, 20);
+            this.n5.TabIndex = 4;
+            // 
+            // n6
+            // 
+            this.n6.Location = new System.Drawing.Point(352, 61);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(100, 20);
+            this.n6.TabIndex = 5;
+            // 
+            // n7
+            // 
+            this.n7.Location = new System.Drawing.Point(352, 111);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(100, 20);
+            this.n7.TabIndex = 6;
+            // 
+            // n8
+            // 
+            this.n8.Location = new System.Drawing.Point(352, 160);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(100, 20);
+            this.n8.TabIndex = 7;
+            // 
+            // n9
+            // 
+            this.n9.Location = new System.Drawing.Point(352, 205);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(100, 20);
+            this.n9.TabIndex = 8;
+            // 
+            // n10
+            // 
+            this.n10.Location = new System.Drawing.Point(352, 250);
+            this.n10.Name = "n10";
+            this.n10.Size = new System.Drawing.Size(100, 20);
+            this.n10.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ingrese numero (1)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ingrese numero (2)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ingrese numero (3)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ingrese numero (4)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ingrese numero (5)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(251, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Ingrese numero (6)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(251, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Ingrese numero (7)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(251, 163);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Ingrese numero (8)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(251, 208);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Ingrese numero (9)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(251, 253);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Ingrese numero (10)";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(27, 324);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(198, 23);
+            this.btn1.TabIndex = 20;
+            this.btn1.Text = "Mostrar el numero mayor y menor";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(254, 324);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(198, 23);
+            this.btn2.TabIndex = 21;
+            this.btn2.Text = "Calcular valor de la Media de toda la serie";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(511, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 134);
+            this.listBox1.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(508, 222);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(114, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Media de toda la serie:";
+            // 
+            // md
+            // 
+            this.md.AutoSize = true;
+            this.md.Location = new System.Drawing.Point(628, 222);
+            this.md.Name = "md";
+            this.md.Size = new System.Drawing.Size(19, 13);
+            this.md.TabIndex = 24;
+            this.md.Text = "00";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(508, 269);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(95, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Numero mayor es: ";
+            // 
+            // maxi
+            // 
+            this.maxi.AutoSize = true;
+            this.maxi.Location = new System.Drawing.Point(628, 269);
+            this.maxi.Name = "maxi";
+            this.maxi.Size = new System.Drawing.Size(19, 13);
+            this.maxi.TabIndex = 26;
+            this.maxi.Text = "00";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(509, 309);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(96, 13);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "Numero menor es: ";
+            // 
+            // mini
+            // 
+            this.mini.AutoSize = true;
+            this.mini.Location = new System.Drawing.Point(628, 309);
+            this.mini.Name = "mini";
+            this.mini.Size = new System.Drawing.Size(19, 13);
+            this.mini.TabIndex = 28;
+            this.mini.Text = "00";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(164, 295);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(147, 23);
+            this.btn.TabIndex = 29;
+            this.btn.Text = "Ingresar numeros";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(452, 386);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(153, 13);
+            this.label12.TabIndex = 30;
+            this.label12.Text = "Promedio de numeros positivos";
+            // 
+            // md2
+            // 
+            this.md2.AutoSize = true;
+            this.md2.Location = new System.Drawing.Point(628, 386);
+            this.md2.Name = "md2";
+            this.md2.Size = new System.Drawing.Size(19, 13);
+            this.md2.TabIndex = 31;
+            this.md2.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 426);
+            this.Controls.Add(this.md2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.mini);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.maxi);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.md);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.n10);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox n1;
+        private System.Windows.Forms.TextBox n2;
+        private System.Windows.Forms.TextBox n3;
+        private System.Windows.Forms.TextBox n4;
+        private System.Windows.Forms.TextBox n5;
+        private System.Windows.Forms.TextBox n6;
+        private System.Windows.Forms.TextBox n7;
+        private System.Windows.Forms.TextBox n8;
+        private System.Windows.Forms.TextBox n9;
+        private System.Windows.Forms.TextBox n10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label md;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label maxi;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label mini;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label md2;
+    }
+}
+
---
+++ Ejercicio1new/Ejercicio1new/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1new
+{
+    public partial class Form1 : Form
+    {
+        private int[] lista = new int[10];
+        private int num1=0, num2 = 0, num3 = 0, num4 = 0, num5 = 0, num6 = 0, num7 = 0, num8 = 0, num9 = 0, num10 = 0;
+
+       
+
+        private Decimal suma = 0, media=0, suma1 = 0, media1 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(n1.Text);
+            num2 = Convert.ToInt32(n2.Text);
+            num3 = Convert.ToInt32(n3.Text);
+            num4 = Convert.ToInt32(n4.Text);
+            num5 = Convert.ToInt32(n5.Text);
+            num6 = Convert.ToInt32(n6.Text);
+            num7 = Convert.ToInt32(n7.Text);
+            num8 = Convert.ToInt32(n8.Text);
+            num9 = Convert.ToInt32(n9.Text);
+            num10 = Convert.ToInt32(n10.Text);
+            listBox1.Items.Clear();
+            lista[0] = num1;
+            lista[1] = num2;
+            lista[2] = num3;
+            lista[3] = num4;
+            lista[4] = num5;
+            lista[5] = num6;
+            lista[6] = num7;
+            lista[7] = num8;
+            lista[8] = num9;
+            lista[9] = num10;
+            for (int i = 0; i != (lista.Length); i++)
+            {
+                listBox1.Items.Add(lista[i]);
+
+            }
+
+            n1.Text = String.Empty;
+            n2.Text = String.Empty;
+            n3.Text = String.Empty;
+            n4.Text = String.Empty;
+            n5.Text = String.Empty;
+            n6.Text = String.Empty;
+            n7.Text = String.Empty;
+            n8.Text = String.Empty;
+            n9.Text = String.Empty;
+            n10.Text = String.Empty;
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            var numeros = listBox1.Items.Cast<object>().Select(obj => Convert.ToInt32(obj));
+            int min = numeros.Min();
+            int max = numeros.Max();
+            mini.Text =  min.ToString();
+            maxi.Text = max.ToString();
+            for (int i = 0; i != (lista.Length); i++)
+            {
+                if(lista[i] > 0)
+                {
+                    suma += lista[i];
+                    media = suma / lista[i];
+                }
+                else
+                {
+                    media = 0;
+                }
+
+            }
+            md2.Text = media.ToString();
+
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i != (lista.Length); i++)
+            {
+                suma += lista[i];
+                media = suma / lista[i];
+
+            }
+            md.Text = media.ToString();
+        }
+    }
+}
