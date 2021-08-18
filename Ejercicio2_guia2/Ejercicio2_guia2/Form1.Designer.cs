df9e829d749f7c7582dde0152b147c2387e09321
Jason Sosa <hernamdezjason36@gmail.com>
2021-08-18 11:46:39 -0600

    Ejercicio 2 nuevo(Ejercicio2_guia2)

---
+++ Ejercicio2_guia2/Ejercicio2_guia2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Ejercicio2_guia2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(324, 156);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(198, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Ingresar";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(217, 93);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(120, 17);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "Ingresar número: ";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(362, 93);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(231, 22);
+            this.txt1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
---
+++ Ejercicio2_guia2/Ejercicio2_guia2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2_guia2
+{
+    public partial class Form1 : Form
+    {
+        private int x;
+        private double[] numero = new double[0];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            x = txt1.Text.Length;
+
+            for(int i = 0; i<x; i++){
+                numero[i] = Convert.ToInt32(txt1.Text)
+
+            }
+
+            
+        }
+    }
+}
