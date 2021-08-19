844445e354b77ad298707ca7a00ce75260b02d1b
Jason Sosa <hernamdezjason36@gmail.com>
2021-08-18 19:34:28 -0600

    Finalización de Ejercicio2_Guia2

--- Ejercicio2_guia2/Ejercicio2_guia2/Form1.cs
+++ Ejercicio2_guia2/Ejercicio2_guia2/Form1.cs
@@ -14,7 +14,8 @@ namespace Ejercicio2_guia2
     {
         public int x;
         public int y;
-        public double[] numero = new double[900];
+        public List<double> Cadena= new List<double>();
+        public double[] numero = new double[9000];
         public double[] NumDes = new double[9000];
         public double suma = 0;
         public double media = 0;
@@ -32,38 +33,14 @@ namespace Ejercicio2_guia2
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            x = txt1.Text.Length;
-            for (int i = 0; i < x; i++)
-            {
-                numero[i] = Convert.ToInt32(txt1.Text);
-                list1.Items.Add(numero[i]);
-                
-                txt1.Text = String.Empty;
-                y +=1 ;
-                
-            }
-            for (int i = 0; i < y; i++)
-            {
-                suma =   numero[i] + suma;
-            }
-
+            Cadena.Add(Convert.ToDouble(txt1.Text));
             
-            media = suma / y;
-            //txt3.Text = y.ToString();
-           
-
-
-
-
-
-
-
-
-
+            txt1.Text = String.Empty;
         }
         private void btn3_Click(object sender, EventArgs e)
         {
-            txt3.Text = media.ToString();
+            media = Cadena.Average();
+            txt3.Text = Cadena.Average().ToString();
 
 
         }
@@ -72,28 +49,14 @@ namespace Ejercicio2_guia2
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            for(int i = 0; i<y; i++)
+            foreach (  double i in Cadena)
             {
-                //sumaDes[i] = Math.Pow((numero[i] - media), 2);
-                DesTipic = DesTipic + Math.Pow(numero[i] - media, 2);
+                DesTipic += Math.Pow(i - media, 2);
             }
-            for(int i = y; i>y; i--)
-            {
-                list2.Items.Add(numero[i]);
-            }
-            //for(int h = 0; h<y; y++)
-            //{
-            //    list2.Items.Add(sumaDes[h].ToString());
-            //}
-
-            //for(int j = 0; j<y; j++)
-            //{
-            //    DesTipic = DesTipic + sumaDes[j];
-            //}
-            //txt4.Text = DesTipic.ToString();
-            DesTipic = DesTipic / y;
-            //DesTipic = Math.Sqrt(DesTipic);
-            TotalDesTipic = DesTipic;
+            
+            
+           
+            TotalDesTipic = Math.Sqrt(DesTipic / Cadena.Count);
             txt2.Text = TotalDesTipic.ToString();
         }
 
